0e980815337c9e91504cc0cb810da3e8a276c2c0
Daniel Xu <danxu@blackberry.com>
2023-08-16 19:30:51 -0400

    Implemented OANDA API for automated trading with placeholder strategy

---
+++ Trader.py
@@ -0,0 +1,201 @@
+import pandas as pd
+import numpy as np
+import tpqoa
+from datetime import datetime, timedelta, timezone
+import time
+
+class Trader(tpqoa.tpqoa):
+    def __init__(self, conf_file, instrument, bar_length, units, window, duration):
+        super().__init__(conf_file)
+        self.instrument = instrument
+        self.bar_length = pd.to_timedelta(bar_length)
+        self.tick_data = pd.DataFrame()
+        self.start = datetime.utcnow()
+        self.end = self.start + timedelta(minutes=duration)
+        self.raw_data = None
+        self.data = None 
+        self.last_bar = None
+        self.units = units
+        self.position = 0
+        self.profits = []
+        self.window = window
+
+        self.start_trade_session()
+    
+    def start_trade_session(self, days=5, max_attempts=None, sleep_period=15, sleep_increase=0):
+        attempt = 0
+        success = False
+        while True:
+            try:
+                self.get_most_recent(days)
+                self.stream_data(self.instrument)
+            except Exception as e:
+                print('Error:', e)
+            else:
+                success = True
+                break
+            finally:
+                attempt += 1
+                print('Attempt: {}'.format(attempt), end='\n')
+                if not success:
+                    if max_attempts is not None and attempt >= max_attempts:
+                        print('MAX ATTEMPTS REACHED')
+                        try:
+                            time.sleep(sleep_period)
+                            self.terminate_session(
+                                cause='TOO MANY ERRORS - SESSION TERMINATED.'
+                            )
+                        except Exception as e:
+                            print('Error:', e)
+                            print('COULD NOT TERMINATE SESSION.')
+                        finally:
+                            break
+                    else: # TRY AGAIN
+                        time.sleep(sleep_period)
+                        sleep_period += sleep_increase
+                        self.tick_data = pd.DataFrame()
+
+    def get_most_recent(self, days=5):
+        time.sleep(1)
+        print('*' * 50)
+        print('ATTEMPTING TO MERGE')
+        print('REQUIRE UNDER {} SECONDS'.format(self.bar_length.seconds))
+        now = datetime.utcnow()
+        now = now - timedelta(microseconds=now.microsecond)
+        past = now - timedelta(days=days)
+        df = (self.get_history(
+                instrument = self.instrument,
+                start = past, 
+                end = now,
+                granularity = 'S5',
+                price = 'M',
+                localize = True
+            )
+            .c.dropna()
+            .to_frame()
+        )
+        df.rename(columns = {'c':self.instrument}, inplace=True)
+        df = df.resample(self.bar_length, label='right').last().dropna().iloc[:-1]
+        self.raw_data = df.copy()
+        self.last_bar = self.raw_data.index[-1]
+        
+        print('Seconds: {}'.format((datetime.utcnow() - self.last_bar).seconds))
+
+        if datetime.utcnow() - self.last_bar >= self.bar_length:
+            print('-----VERIFY THAT BOT IS RUNNING DURING TRADING HOURS-----')
+            self.get_most_recent()
+        else:
+            print('SUCCESSFULLY MERGED')
+            print('*' * 50)
+
+    def close_position(self):
+        if self.position == 1:
+            order = self.create_order(
+                self.instrument,
+                -self.units,
+                suppress = True,
+                ret = True
+            )
+            self.report_trade(order, 'GOING NEUTRAL')
+        elif self.position == -1:
+            order = self.create_order(
+                self.instrument,
+                self.units,
+                suppress = True,
+                ret = True
+            )
+            self.report_trade(order, 'GOING NEUTRAL')
+        print('\nSESSION OVER')
+        self.position = 0
+
+    def end_trade_session(self):
+        self.stop_stream = True
+
+        if self.position != 0:
+            close_order = self.create_order(
+                self.instrument,
+                units = -self.position * self.units,
+                suppress = True,
+                ret = True
+            )
+            self.report_trade(close_order, "GOING NEUTRAL")
+            self.position = 0
+        print('\nENDING TRADING SESSION...')
+
+    def on_success(self, time, bid, ask):
+        recent_tick = pd.to_datetime(time).replace(tzinfo=None)
+        print(self.ticks, end=' ')
+        if recent_tick >= self.end:
+            self.end_trade_session()
+            return
+
+        df = pd.DataFrame({self.instrument:(ask + bid) / 2}, index=[recent_tick])
+        self.tick_data = pd.concat([self.tick_data, df])
+        
+        if self.ticks == 1 or recent_tick - self.last_bar > self.bar_length:
+            self.resample_and_join()
+            self.define_strategy()
+            self.execute_trades()
+    
+    def resample_and_join(self):
+        self.raw_data = pd.concat([
+            self.raw_data, 
+            self.tick_data.resample(self.bar_length, label="right").last().ffill().iloc[:-1]
+        ])
+        self.tick_data = self.tick_data.iloc[-1:]
+        self.last_bar = self.raw_data.index[-1]
+    
+    def define_strategy(self): # "strategy-specific"
+        df = self.raw_data.copy()
+        
+        #******************** define your strategy here ************************
+        df["returns"] = np.log(df[self.instrument] / df[self.instrument].shift())
+        df["position"] = -np.sign(df.returns.rolling(self.window).mean())
+        #***********************************************************************
+        
+        self.data = df.copy()
+    
+    def execute_trades(self):
+        if self.data['position'].iloc[-1] == 1:
+            if self.position == 0:
+                order = self.create_order(self.instrument, self.units, suppress=True, ret=True)
+                self.report_trade(order, 'GOING LONG')
+            elif self.position == -1:
+                order = self.create_order(self.instrument, self.units * 2, suppress=True, ret=True) 
+                self.report_trade(order, 'GOING LONG')
+            else:
+                print('STAYING LONG')
+            self.position = 1
+        elif self.data['position'].iloc[-1] == -1: 
+            if self.position == 0:
+                order = self.create_order(self.instrument, -self.units, suppress=True, ret=True)
+                self.report_trade(order, 'GOING SHORT')
+            elif self.position == 1:
+                order = self.create_order(self.instrument, -self.units * 2, suppress=True, ret=True)
+                self.report_trade(order, 'GOING SHORT')
+            else:
+                print('STAYING SHORT')
+            self.position = -1
+        elif self.data['position'].iloc[-1] == 0:
+            if self.position == -1:
+                order = self.create_order(self.instrument, self.units, suppress=True, ret=True) 
+                self.report_trade(order, 'GOING NEUTRAL')
+            elif self.position == 1:
+                order = self.create_order(self.instrument, -self.units, suppress=True, ret=True) 
+                self.report_trade(order, 'GOING NEUTRAL')
+            else:
+                print('STAYING NEUTRAL')
+            self.position = 0
+    
+    def report_trade(self, order, going):
+        time = order["time"]
+        units = order["units"]
+        price = order["price"]
+        pl = float(order["pl"])
+        self.profits.append(pl)
+        cumpl = sum(self.profits)
+        print("\n" + 50* '*')
+        print("{} | {}".format(time, going))
+        print("{} | Units = {} | Price = {} | P&L = {} | Cumulative P&L = {}".format(time, units, price, pl, cumpl))
+        print(50 * '*' + "\n")  
+    
